a46bb5714f72e743652ad937b95823c6a255613a
GaneshChintalapaty <ganesh.chintalapaty@gmail.com>
2024-02-06 10:54:48 -0600

    CUADGC_06022024_1054

--- CUADGC.Designer.cs
+++ CUADGC.Designer.cs
@@ -37,20 +37,22 @@
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.sendCANFDBytes = new System.Windows.Forms.Button();
+            this.sendUARTBytes = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.readCANFDUARTBytes = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.controlDAC1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.controlDAC2)).BeginInit();
             this.SuspendLayout();
             // 
             // controlGPIO1
             // 
-            this.controlGPIO1.BackColor = System.Drawing.Color.Red;
+            this.controlGPIO1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.controlGPIO1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.controlGPIO1.Location = new System.Drawing.Point(344, 56);
+            this.controlGPIO1.Location = new System.Drawing.Point(366, 70);
             this.controlGPIO1.Name = "controlGPIO1";
             this.controlGPIO1.Size = new System.Drawing.Size(91, 23);
             this.controlGPIO1.TabIndex = 0;
@@ -59,10 +61,10 @@
             // 
             // controlGPIO2
             // 
-            this.controlGPIO2.BackColor = System.Drawing.Color.Red;
+            this.controlGPIO2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.controlGPIO2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.controlGPIO2.ForeColor = System.Drawing.Color.Black;
-            this.controlGPIO2.Location = new System.Drawing.Point(441, 56);
+            this.controlGPIO2.Location = new System.Drawing.Point(463, 70);
             this.controlGPIO2.Name = "controlGPIO2";
             this.controlGPIO2.Size = new System.Drawing.Size(91, 23);
             this.controlGPIO2.TabIndex = 1;
@@ -73,7 +75,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(395, 31);
+            this.label1.Location = new System.Drawing.Point(418, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 2;
@@ -81,23 +83,23 @@
             // 
             // controlDAC1
             // 
-            this.controlDAC1.Location = new System.Drawing.Point(344, 157);
+            this.controlDAC1.Location = new System.Drawing.Point(373, 170);
             this.controlDAC1.Name = "controlDAC1";
-            this.controlDAC1.Size = new System.Drawing.Size(188, 45);
+            this.controlDAC1.Size = new System.Drawing.Size(216, 45);
             this.controlDAC1.TabIndex = 3;
             // 
             // controlDAC2
             // 
-            this.controlDAC2.Location = new System.Drawing.Point(344, 221);
+            this.controlDAC2.Location = new System.Drawing.Point(373, 234);
             this.controlDAC2.Name = "controlDAC2";
-            this.controlDAC2.Size = new System.Drawing.Size(188, 45);
+            this.controlDAC2.Size = new System.Drawing.Size(216, 45);
             this.controlDAC2.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(395, 121);
+            this.label2.Location = new System.Drawing.Point(424, 135);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 13);
             this.label2.TabIndex = 5;
@@ -107,7 +109,7 @@
             // 
             this.elementHost1.Location = new System.Drawing.Point(12, 12);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(297, 283);
+            this.elementHost1.Size = new System.Drawing.Size(300, 300);
             this.elementHost1.TabIndex = 6;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = null;
@@ -116,7 +118,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 321);
+            this.label3.Location = new System.Drawing.Point(18, 348);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
             this.label3.TabIndex = 7;
@@ -124,64 +126,86 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 318);
+            this.textBox1.Location = new System.Drawing.Point(114, 345);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(327, 20);
+            this.textBox1.Size = new System.Drawing.Size(394, 20);
             this.textBox1.TabIndex = 8;
             // 
-            // button1
+            // sendCANFDBytes
             // 
-            this.button1.Location = new System.Drawing.Point(441, 316);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.sendCANFDBytes.Location = new System.Drawing.Point(514, 345);
+            this.sendCANFDBytes.Name = "sendCANFDBytes";
+            this.sendCANFDBytes.Size = new System.Drawing.Size(75, 23);
+            this.sendCANFDBytes.TabIndex = 9;
+            this.sendCANFDBytes.Text = "Send";
+            this.sendCANFDBytes.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // sendUARTBytes
             // 
-            this.button2.Location = new System.Drawing.Point(441, 353);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.sendUARTBytes.Location = new System.Drawing.Point(514, 382);
+            this.sendUARTBytes.Name = "sendUARTBytes";
+            this.sendUARTBytes.Size = new System.Drawing.Size(75, 23);
+            this.sendUARTBytes.TabIndex = 12;
+            this.sendUARTBytes.Text = "Send";
+            this.sendUARTBytes.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(108, 355);
+            this.textBox2.Location = new System.Drawing.Point(114, 382);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(327, 20);
+            this.textBox2.Size = new System.Drawing.Size(394, 20);
             this.textBox2.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 358);
+            this.label4.Location = new System.Drawing.Point(18, 385);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 13);
             this.label4.TabIndex = 10;
             this.label4.Text = "UART Write:";
             // 
-            // listBox1
+            // readCANFDUARTBytes
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 404);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(520, 173);
-            this.listBox1.TabIndex = 13;
+            this.readCANFDUARTBytes.FormattingEnabled = true;
+            this.readCANFDUARTBytes.Location = new System.Drawing.Point(18, 431);
+            this.readCANFDUARTBytes.Name = "readCANFDUARTBytes";
+            this.readCANFDUARTBytes.Size = new System.Drawing.Size(571, 173);
+            this.readCANFDUARTBytes.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(324, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "DAC1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(324, 243);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "DAC2:";
             // 
             // CUADGC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 589);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(608, 624);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.readCANFDUARTBytes);
+            this.Controls.Add(this.sendUARTBytes);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sendCANFDBytes);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.elementHost1);
@@ -212,11 +236,13 @@
         private System.Windows.Forms.Integration.ElementHost elementHost1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button sendCANFDBytes;
+        private System.Windows.Forms.Button sendUARTBytes;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox readCANFDUARTBytes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
